8612f2ac0661bbeae231a684ce123cb240cede27
tluthanhnx <thanhnx@thanglong.edu.vn>
2025-07-16 16:53:07 +0700

    COMMIT

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from source.detect_drift.detect import detect_drift
 from source.Agent.explan_with_ai import explan_drift,select_better_response
 from source.send_email.send import send_email_to_member
 from dotenv import load_dotenv
-load_dotenv('.env')
+load_dotenv('./source/.env')
 
 path_fide_planJson = 'plan.json'
 
