5cc62669aed2f0f102cd98413f1d89d59754793b
tluthanhnx <thanhnx@thanglong.edu.vn>
2025-07-16 16:17:46 +0700

    CCC

--- demo.py
+++ demo.py
@@ -8,18 +8,6 @@ load_dotenv('./source/.env')
 api_key =  os.getenv('open_router_api_key')
 print(api_key)
 
-# with open('plan.json','r') as f:
-#     resource_changes = json.load(f)
-#     resource_changes = resource_changes['resource_changes']
-#     new_resource = resource_changes[2]['change']['after']
-#     old_resource = resource_changes[2]['change']['before']
-#     action = resource_changes[2]['change']['actions']
-# print(len(resource_changes))
-# print(action)
-# print(new_resource.keys())
-# print()
-# print(old_resource.keys())
-# print('___________________') plan.json
 with open("plan.json", "r", encoding="utf-8") as f:
     content = f.read()
     
@@ -82,53 +70,6 @@ completion = client.chat.completions.create(
 result1  = completion.choices[0].message.content
 print(result1)
 
-# completion = client.chat.completions.create(
-  
-#   model="deepseek/deepseek-r1-0528:free",
-#   messages=messages,
- 
-# )
-# result2 = completion.choices[0].message.content
-
-# print('sssssss')
-# result = {
-#     'result1' : result1,
-#     'result2': result2
-# }
-
-
-# messages = [
-#     {'role':'system', 'content':f"""Bạn là một chuyên gia DevOps và reviewer trong một cuộc thi về hệ thống phát hiện drift Terraform.
-
-# Dưới đây là **2 câu trả lời từ hai mô hình AI khác nhau** cho cùng một tình huống drift.  
-
-# Hãy phân tích và đánh giá chúng theo các tiêu chí sau.phân tích trong thầm nặng và trả ra kết quả result1 hay result2:
-
-# 1. Câu trả lời nào **đúng hơn về kỹ thuật**?
-# 2. Câu trả lời nào **giải thích rõ ràng hơn**?
-# 3. Câu trả lời nào **đưa ra khuyến nghị hợp lý hơn**?
-# 4. Có lỗi hoặc thông tin không chính xác nào không?
-
-# Sau đó:
-
-# - Phản hồi theo dạng json cái nào tốt hơn lên trước ví dụ result1 tốt hơn result 2 thì trả ra array có dạng. Không trả về bát cứ lời giải thích nào chỉ cần trả về danh sách xếp hạng của các câu trả lời
-
-# """
-#     },
-#     {'role':'user', 'content':f""" Tôi sẽ cung cap  ket qua. 
-#                                       {result}
-#                                   """
-#     }
-# ]
-# completion = client.chat.completions.create(
-  
-#   model="qwen/qwen3-235b-a22b:free",
-#   messages=messages
-# )
-# print(completion.choices[0].message.content)
-# print(result['completion.choices[0].message.content'])
-
-
 import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
@@ -137,7 +78,7 @@ import markdown
 result1 = markdown.markdown(result1)
 message = Mail(
     from_email='nhphan.ai01@gmail.com',           # Địa chỉ email của bạn (phải được xác thực với SendGrid)
-    to_emails='phannguyenhuu46@gmail.com;thanhnx1979@gmail.com',        # Người nhận
+    to_emails=['phannguyenhuu46@gmail.com', 'thanhnx1979@gmail.com']
     subject='Detect Drift',
     html_content= result1
 )
